640357a1416a6a01e997e59a9e9ce8221fa3d206
GulruIbrahimi <gulruibrahimi@gmail.com>
2023-08-19 17:56:31 +0300

    first commit

---
+++ src/agustos14/Runner.java
@@ -0,0 +1,8 @@
+package agustos14;
+
+public class Runner {
+    public static void main(String[] args) {
+        Menu menu=new Menu();
+        menu.menu();
+    }
+}
---
+++ src/agustos14/User.java
@@ -0,0 +1,17 @@
+package agustos14;
+
+import java.time.LocalDateTime;
+
+public class User {
+    // 1- Bir user(Kullanıcı) class oluşturun fields: name , registerDate(kayıtZamanı) (LocalDateTime cinsinden)
+    String name;
+    LocalDateTime registerDate;
+
+    public User() { //p'siz cons. oluşturuldu
+    }
+
+    public User(String name, LocalDateTime registerDate) { // p'li cons. oluşturuldu
+        this.name = name;
+        this.registerDate = registerDate;
+    }
+}
